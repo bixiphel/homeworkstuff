fac927b74dea0c6efd62e962def981c04c0b3c21
bixiphel <146150116+bixiphel@users.noreply.github.com>
2024-07-28 21:53:04 -0500

    Add files via upload
    This was a little programming assignment working with logical programming and using the Prolog language. The assignment was to create 3 functions: a motherInLaw(X,Y) function that determines if X is Y's mother-in-law, a grandParent(X,Y) function which determines if X is Y's grandparent, and a grandMother(X,Y) function which determines if X is Y's grandmother.

---
+++ Family.pl
@@ -0,0 +1,84 @@
+% Completed by: Jack Allen
+
+% Here are some men
+man(stuart).
+man(kurt).
+man(mark).
+man(raymond).
+man(august).
+man(hans).
+man(ryan).
+man(adam).
+man(max).
+
+% Here are some women.
+woman(erica).
+woman(susan).
+woman(elaine).
+woman(viola).
+woman(ida).
+woman(florence).
+woman(marissa).
+woman(annika).
+woman(kaita).
+
+% For our purposes, we will always put the woman's name first in the
+% marriage facts.
+married(erica, stuart).
+married(susan, mark).
+married(elaine, raymond).
+married(viola, august).
+married(ida, hans).
+married(marissa, ryan).
+married(annika, adam).
+
+% For our purposes, the parent's name always comes first
+parent(susan, marissa).
+parent(mark, marissa).
+parent(susan, annika).
+parent(mark, annika).
+parent(elaine, mark).
+parent(raymond, mark).
+parent(elaine, stuart).
+parent(raymond, stuart).
+parent(elaine, kurt).
+parent(raymond, kurt).
+parent(viola, elaine).
+parent(august, elaine).
+parent(ida, raymond).
+parent(hans, raymond).
+
+
+
+
+
+% Returns true if X is Y's mother-in-law
+motherInLaw(X,Y) :- 
+    married(Q, Y), % Checks if Y is married
+    woman(X), % Checks if X is in the list of women
+    parent(X, Q). % Checks if X is Q's parent
+
+% Checks if X is Y's grandparent
+grandParent(X,Y) :- 
+    parent(X,Z), % Checks if X is Z's parent
+    parent(Z, Y). % Checks if Z is Y's parent
+
+% Checks if X is Y's grandmother
+grandMother(X,Y) :- 
+    parent(X,Z), % Checks if X is Z's parent
+    parent(Z, Y), % Checks if Z is Y's parent
+    woman(X). % Checks if X is in the list of women
+
+
+
+
+
+
+
+
+
+
+
+
+
+
